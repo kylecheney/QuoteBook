e3a207aacb9c307cb9a41a7a6f60d0fc178c1ba3
kylecheney <kylejosephcheney@gmail.com>
2016-11-15 17:01:44 -0700

    finished filter, add and remove functionality for quoteBook

--- app/js/mainCtrl.js
+++ app/js/mainCtrl.js
@@ -1,5 +1,20 @@
-angular.module('quoteBook', []).controller('mainCtrl', function($scope, mainServ) {
+angular.module('quoteBook').controller('mainCtrl', function($scope, mainServ) {
 
-  
+  $scope.quotes = mainServ.getQuotes();
+
+  $scope.addQuote = function (addText, addAuthor) {
+    var newQuote = {
+      text : addText,
+      author : addAuthor
+    }
+    mainServ.addQuote(newQuote);
+    $scope.addText = '';
+    $scope.addAuthor = '';
+  }
+
+  $scope.removeQuote = function (index) {
+    console.log(index);
+    mainServ.removeQuote(index);
+  }
 
 })
